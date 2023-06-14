9abd9d1aee9b2eedf4aa3730de8e25a54858f38e
Valery <valera.sawer@gmail.com>
2023-06-14 21:15:37 +0300

    14.06 \ 21:15

--- script/popup.js
+++ script/popup.js
@@ -1,12 +1,12 @@
-let showPopup = document.getElementById("pop-cont");
-let closePopup = document.getElementById("x");
+// let showPopup = document.getElementById("pop-cont");
+// let closePopup = document.getElementById("x");
 
-function show() {
-    showPopup.style.display = "block"; 
-}
-function hide() {
-    showPopup.style.display = "none"; 
-}
+// function show() {
+//     showPopup.style.display = "block"; 
+// }
+// function hide() {
+//     showPopup.style.display = "none"; 
+// }
 
-document.body.onclick = show;
-document.closePopup.onclick = hide;
+// document.body.onclick = show;
+// document.closePopup.onclick = hide;
